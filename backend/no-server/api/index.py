e6471bf3ddbd25fb3e3744ceae37af8ca1cee5f9
Shreyas Papinwar <spapinwar@gmail.com>
2023-02-08 18:21:30 +0530

    test uniswap-check-loss

--- backend/no-server/api/index.py
+++ backend/no-server/api/index.py
@@ -2,37 +2,39 @@ from sanic import Sanic
 from sanic.response import json
 import os
 import pymongo
+import requests
 
 app = Sanic()
 
-
 client = pymongo.MongoClient("mongodb+srv://vercel:" + os.environ.get("MONGO_PASSWORD") + "@cluster0.1ygibls.mongodb.net/?retryWrites=true&w=majority")
-db = client.test
+db = client.networks
 
 @app.route('/')
 @app.route('/<path:path>')
 async def index(request, path=""):
     return json({'hello': path})
 
-@app.route('/aave-add-user', methods=['PUT'])
-async def aave_add_user(request):
-    # Get the address and network from the request
-    address = request.json.get('address')
-    network = request.json.get('network')
+@app.route('/uniswap-check-loss', methods=['PUT'])
+async def uniswap_check_impermanent_loss(request):
 
-    # check db copnnection
-    if db is not None:
-        # Insert the user into the database
-        db.networks.insert_one({
-            'address': address,
-            'network': network
+    graph_url = request.json.get('graph_url')
+    response = requests.post(graph_url, json={
+            'query': '''
+                query {
+                    userPositions(where: { check: true }) {
+                        id
+                        tokenId
+                        token0
+                        token1
+                        check
+                    }
+                }
+            '''
         })
 
-        # Return the success response
-        return json({
-            'success': True
-        })
-    else:
-        return json({
-            'success': False
-        })
+    data = response.json()['data']['userPositions']
+    
+    return json({
+        'success': True,
+        'data': data
+    })
