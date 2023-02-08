13bfab19c8497162551b790879d72d3405970201
Shreyas Papinwar <spapinwar@gmail.com>
2023-02-08 17:33:36 +0530

    Add mongo vercel

--- backend/no-server/api/index.py
+++ backend/no-server/api/index.py
@@ -1,9 +1,38 @@
 from sanic import Sanic
 from sanic.response import json
+import os
+import pymongo
+
 app = Sanic()
 
+
+client = pymongo.MongoClient("mongodb+srv://vercel:" + os.environ.get("MONGO_PASSWORD") + "@cluster0.1ygibls.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
 
 @app.route('/')
 @app.route('/<path:path>')
 async def index(request, path=""):
     return json({'hello': path})
+
+@app.route('/aave-add-user', methods=['PUT'])
+async def aave_add_user(request):
+    # Get the address and network from the request
+    address = request.json.get('address')
+    network = request.json.get('network')
+
+    # check db copnnection
+    if db:
+        # Insert the user into the database
+        db.users.insert_one({
+            'address': address,
+            'network': network
+        })
+
+        # Return the success response
+        return json({
+            'success': True
+        })
+    else:
+        return json({
+            'success': False
+        })
